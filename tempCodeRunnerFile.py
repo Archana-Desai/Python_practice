7e8ecc25c1d7691f58454b12cd1152cfb48bf2b4
Archana-Desai <archanadesai634@gmail.com>
2026-04-19 21:10:01 +0530

    removed unwanted files

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-if(computer==you):
-    print("It's a Draw!")
-else:
-    if((computer-you)==-1 or (computer-you)==2):
-        print(print("You Loose !"))
-    elif((computer-you)==1 or (computer-you)==-2):
-        print("You won!")
-    else:
-        print("Something wrong in the input ")
